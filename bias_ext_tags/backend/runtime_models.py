ba4597f319741461b92ca902371dc38946117f6a
woniu <2463396298@qq.com>
2026-06-29 15:06:39 +0800

    Reference content discussion model

--- bias_ext_tags/backend/runtime_models.py
+++ bias_ext_tags/backend/runtime_models.py
@@ -1,5 +1,5 @@
 from bias_core.extensions import RuntimeModel
 
 
-DISCUSSION_MODEL = RuntimeModel("discussions.service", description="discussions 扩展提供的讨论模型。")
+DISCUSSION_MODEL = RuntimeModel("content.discussions", description="content 基础包提供的讨论模型。")
 POST_MODEL = RuntimeModel("content.posts", description="content 基础包提供的帖子模型。")
